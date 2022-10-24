fba4d7557936083b473894d6470e62e06e24d841
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-10-24 23:48:22 +0400

    Time: 49 ms (39.08%), Space: 13.5 MB (82.04%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -5,19 +5,24 @@ class Solution(object):
         :type message: str
         :rtype: str
         """
-        KEY = ""
-        for i in key:
-            if KEY.find(i) == -1 and i != ' ':
-                KEY += i
-       
         alphabets = "abcdefghijklmnopqrstuvwxyz"
-        output = ''
+        modifiedKey = ""
+        for i in key:
+            if i not in modifiedKey and i != ' ':
+                modifiedKey += i
+        Map = {}
+        for i in range(26):
+            Map[modifiedKey[i]] = alphabets[i]
+        output = ""
         for i in message:
             if i == ' ':
                 output += ' '
             else:
-                for j in range(26):
-                    if i == KEY[j]:
-                        output += alphabets[j] 
+                output += Map[i]
         return output
+            
+        
+            
+        
+            
             
