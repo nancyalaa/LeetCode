836a47c725c44412f6cc47fd5a9ee04f14d57c2e
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-10-27 00:58:52 +0400

    Time: 30 ms (80.13%), Space: 13.4 MB (86.54%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        temp = set(s)
         flag = 0
-        for i in s:
+        for i in temp:
             if s.count(i) == s.count(s[0]):
                 continue
             else:
                 flag = 1
                 break
-            
-        if flag == 1:
+        if flag:
             return False
         else:
             return True
