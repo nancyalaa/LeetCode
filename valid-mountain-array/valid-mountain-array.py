1985fbd703b21063ebcbc57978dc256dd53ead59
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-11-08 21:11:04 +0400

    Time: 5529 ms, Memory: 15.4 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) <= 2:
+            return False
+        if arr.index(max(arr)) == 0 or arr.index(max(arr)) == len(arr)-1:
+            return False
+        flag = True
+        for i in range(arr.index(max(arr))):
+            if arr[i] < arr[i+1]:
+                continue
+            else:
+                return False
+        j = len(arr)-1
+        for i in range(len(arr)-arr.index(max(arr))):
+            if j == arr.index(max(arr)):
+                break
+            if arr[j] < arr[j-1]:
+                j -= 1
+                continue
+            else:
+                return False
+            
+        return True
+                       
