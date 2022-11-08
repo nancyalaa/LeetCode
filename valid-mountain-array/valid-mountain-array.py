b88f46a01eea99e810fd7dfb2f886d504b740559
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-11-08 21:28:01 +0400

    Time: 7648 ms, Memory: 15.4 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -1,16 +1,17 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) <= 2:
+        if len(arr) <= 2 or arr.index(max(arr)) == 0 or arr.index(max(arr)) == len(arr)-1:
             return False
-        if arr.index(max(arr)) == 0 or arr.index(max(arr)) == len(arr)-1:
-            return False
-        flag = True
+        
+        # from the bottom to the top
         for i in range(arr.index(max(arr))):
             if arr[i] < arr[i+1]:
                 continue
             else:
                 return False
+            
         j = len(arr)-1
+        # from the top top
         for i in range(len(arr)-arr.index(max(arr))):
             if j == arr.index(max(arr)):
                 break
