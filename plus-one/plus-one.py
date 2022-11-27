eb0b5c838b2829789ff6151fe4358433683384f2
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-11-28 02:02:46 +0400

    Time: 58 ms, Memory: 14 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,15 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        sumOfDigits = 0
-        power = len(digits)-1
+        s = ""
         for i in digits:
-            sumOfDigits += ((10**power) * i)
-            power -= 1
-        sumOfDigits += 1
-        result = []
-        while(sumOfDigits > 0):
-            result.append(sumOfDigits % 10)
-            sumOfDigits //= 10
-        result.reverse()
-        return result
+            s += str(i)
+        nums = int(s)
+        nums = nums+1
+        s2 = str(nums)
+        L = []
+        for i in s2:
+            j = int(i)
+            L.append(j)
+        return L
         
