dfd0ab0070907805709f7a56c9b6e436c4c607ab
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-12-01 15:20:53 +0400

    Time: 3694 ms (5.05%), Space: 17.8 MB (49.13%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,33 +1,26 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        numberOfRows = len(mat)
-        numberOfCols = len(mat[0])
+        if len(mat) == 0:
+            return []
+        if len(mat[0]) == 1:
+            result = []
+            for i in mat:
+                result.extend(i)
+            return result
+        if len(mat) == 1:
+            result = []
+            for i in mat[0]:
+                result.append(i)
+            return result
         result = []
-        currentRow = 0
-        currentCol = 0
-        toUp = True
-        while len(result) != numberOfRows * numberOfCols:
-            if toUp:
-                while currentRow >= 0 and currentCol < numberOfCols:
-                    result.append(mat[currentRow][currentCol])
-                    currentRow -= 1
-                    currentCol += 1
-                if currentCol == numberOfCols:
-                    currentCol -= 1
-                    currentRow += 2
-                else:
-                    currentRow += 1
-                toUp = False
-            else:
-                while currentRow < numberOfRows and currentCol >= 0:
-                    result.append(mat[currentRow][currentCol])
-                    currentRow += 1
-                    currentCol -= 1
-                if currentRow == numberOfRows:
-                    currentRow -= 1
-                    currentCol += 2
-                else:
-                    currentCol += 1
-                toUp = True
+        for k in range(len(mat)+len(mat[0])):
+            temp = []
+            for i in range(len(mat)):
+                for j in range(len(mat[0])):
+                    if i + j == k:
+                        temp.append(mat[i][j])
+            if k % 2 == 0:
+                temp.reverse()
+            result.extend(temp)
+                        
         return result
-                
