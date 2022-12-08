4232ff3092b5774643ced5b3432d12c9be74166c
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-12-08 06:53:20 +0400

    Time: 140 ms (7.93%), Space: 14.2 MB (46.85%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        sMapt = {}
+        tMaps = {} 
+        for i in range(len(s)):
+            if s[i] not in sMapt.keys():
+                sMapt[s[i]] = t[i]
+            else:
+                if sMapt[s[i]] == t[i]:
+                    continue
+                else:
+                    return False
+        for i in range(len(t)):
+            if t[i] not in tMaps.keys():
+                tMaps[t[i]] = s[i]
+            else:
+                if tMaps[t[i]] == s[i]:
+                    continue
+                else:
+                    return False
+        return True
+                
+        
