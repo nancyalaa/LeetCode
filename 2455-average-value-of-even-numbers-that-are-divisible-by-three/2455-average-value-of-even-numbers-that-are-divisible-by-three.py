fecbcc66b55b9fa8aba4a1a3e6a58d9631f79d65
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2022-12-14 22:59:22 +0400

    Time: 75 ms (99.03%), Space: 14 MB (93.49%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -3,7 +3,7 @@ class Solution:
         cntr = 0
         evenSum = 0
         for i in nums:
-            if i % 2 == 0 and i % 3 == 0:
+            if i % 6 == 0:
                 evenSum += i
                 cntr += 1
         if cntr: return evenSum // cntr
