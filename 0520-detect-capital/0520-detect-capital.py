3c9727046d50c8071d327dfb10911494a0278407
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2023-01-02 23:23:12 +0400

    Time: 37 ms (76.80%), Space: 13.9 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,10 +1,3 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        if word == word.upper():
-            return True
-        elif word == word.lower():
-            return True
-        elif word == word[0].upper() + word[1:].lower():
-            return True
-        return False
-        
+        return word.isupper() or word.islower() or word.istitle()
