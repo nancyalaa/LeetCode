8885c4056864933515c49b802a14a7b4325787f3
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2023-01-24 00:13:40 +0400

    Time: 71 ms (45.02%), Space: 14.1 MB (26.38%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
         arr.sort() 
-        return sum(arr[5*len(arr)//100: len(arr)-5*len(arr)//100])/len(arr[5*len(arr)//100: len(arr)-5*len(arr)//100])
+        number_of_elements_to_be_erased = 5*len(arr)//100
+        return sum(arr[number_of_elements_to_be_erased: len(arr)-number_of_elements_to_be_erased])/len(arr[number_of_elements_to_be_erased: len(arr)-number_of_elements_to_be_erased])
         
         
