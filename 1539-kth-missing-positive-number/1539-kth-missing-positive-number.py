d6a74558f243bfe7db9fb8ac7ac20f17e600d364
Nancy Alaa <72484236+nancyalaa@users.noreply.github.com>
2023-03-06 21:55:51 +0400

    Time: 378 ms (5.05%), Space: 31.9 MB (13.03%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,7 +1,7 @@
 import numpy
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        nums = list(numpy.arange(1, 100000))
+        nums = list(numpy.arange(1, arr[len(arr)-1]+k+1))
         for num in arr: 
             nums.pop(nums.index(num))
         return nums[k-1]
